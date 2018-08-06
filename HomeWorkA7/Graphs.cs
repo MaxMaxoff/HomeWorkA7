8ed62c303c51a3857cdcce52b48e687700da5377
Maxim VT <maxim.vt@gmail.com>
2018-08-06 08:14:51 +0800

    Add project files.

---
+++ HomeWorkA7/Graphs.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HomeWorkA7
+{
+    class Graphs
+    {
+        /// <summary>
+        /// adjacency matrix
+        /// </summary>
+        int[,] graph;
+
+        /// <summary>
+        /// array of vertexes
+        /// </summary>
+        string[] vertex;
+
+        /// <summary>
+        /// Initialize graph with default size 10x10
+        /// </summary>
+        public Graphs()
+        {
+            graph = new int[10, 10];
+            vertex = new string[10];
+        }
+
+        /// <summary>
+        /// Initialize graph with requested size
+        /// </summary>
+        /// <param name="n">size of graph</param>
+        public Graphs(int n)
+        {
+            graph = new int[n, n];
+            vertex = new string[n];
+        }
+
+        /// <summary>
+        /// Initialize graph with requested size and read data from file
+        /// </summary>
+        /// <param name="n">size of graph</param>
+        /// <param name="filename">filename</param>
+        public Graphs(int n, string filename):this(n)
+        {
+            readMatrixFromFile(filename);
+        }
+        
+
+        /// <summary>
+        /// Method read matrix from file
+        /// </summary>
+        /// <param name="filename">name of the file</param>
+        /// <returns>true/false</returns>
+        public bool readMatrixFromFile(string filename)
+        {
+            int i = 0;
+            string line;
+
+            StreamReader streamReader = null;
+
+            try
+            {
+                streamReader = new StreamReader(filename);
+
+                while (!streamReader.EndOfStream)
+                {
+                    try
+                    {
+                        line = streamReader.ReadLine();
+                        string[] fields = line.Split(',');
+                        vertex[i] = fields[0];
+                        for (int j = 1; j < fields.Length; j++)
+                            int.TryParse(fields[j], out graph[j, i]);
+                    }
+                    catch
+                    {
+
+                    }
+                    i++;
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Something goes wrong! Please check the file {filename}");
+                return false;
+            }
+            finally
+            {
+                streamReader?.Close();
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Method write matrix to file
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public bool writeMatrixTofile(string filename)
+        {
+
+            StreamWriter streamWriter = null;
+            string str = "0";
+
+            try
+            {
+                streamWriter = new StreamWriter(filename);
+
+                for (int j = 1; j < vertex.Length; j++)
+                    str += $",{vertex[j]}";
+
+                for (int j = 1; j < vertex.Length; j++)
+                {
+                    str += $"\r\n{vertex[j]}";
+                    for (int i = 1; i < vertex.Length; i++)
+                    {
+                        str += $",{graph[i, j]}";
+                    }
+                }
+
+                streamWriter.WriteLine(str);
+
+            }
+            catch (DirectoryNotFoundException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something goes wrong!");
+                return false;
+            }
+            finally
+            {
+                //if (streamWriter!=null)
+                //streamWriter.Close();
+                streamWriter?.Close();
+
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Method print out adjacency matrix
+        /// </summary>
+        /// <returns>adjacency matrix</returns>
+        public override string ToString()
+        {
+            string str = "    ";
+
+            for (int j = 1; j < vertex.Length; j++)
+                str += $"{vertex[j],3}";
+
+            for (int j = 1; j < vertex.Length; j++)
+            {
+                str += $"\n {vertex[j],3}";
+                for (int i = 1; i < vertex.Length; i++)
+                {
+                    str += $"{graph[i, j],3}";
+                }
+            }
+            return str;
+        }
+    }
+}
---
+++ HomeWorkA7/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+// Библиотека для упрощения работы с консолью.
+// https://github.com/MaxMaxoff/SupportLibrary
+using SupportLibrary;
+
+/// <summary>
+/// Максим Торопов
+/// Ярославль
+/// https://github.com/MaxMaxoff
+/// 
+/// Домашняя работа "Алгоритмы и структуры данных"
+/// 7 урок
+/// </summary>
+namespace HomeWorkA7
+{
+    class Program
+    {
+        /// <summary>
+        /// 1. Написать функции, которые считывают матрицу смежности из файла и выводят ее на экран
+        /// </summary>
+        static void Task1()
+        {
+            string filename = "./matrix.txt";
+
+            int lineCount = TotalLines(filename);
+
+            Graphs graph = new Graphs(lineCount, filename);
+
+            graph.writeMatrixTofile("./matrix2.txt");
+
+            SupportMethods.Pause(graph.ToString());           
+
+        }
+
+        /// <summary>
+        /// function Lines in file
+        /// </summary>
+        /// <param name="filePath">filename</param>
+        /// <returns>lines qty</returns>
+        static int TotalLines(string filePath)
+        {
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                int i = 0;
+                while (r.ReadLine() != null) { i++; }
+                if (r != null) r.Close();
+                return i;
+            }
+        }
+
+        /// <summary>
+        /// 2. Написать рекурсивную функцию обхода графа в глубину.
+        /// </summary>
+        static void Task2()
+        {
+
+        }
+
+        /// <summary>
+        /// 3. Написать функцию обхода графа в ширину.
+        /// </summary>
+        static void Task3()
+        {
+            // try-catch section
+            try
+            {
+                string filename = "./matrix.txt";
+
+                int lineCount = TotalLines(filename);
+
+                Graphs graph = new Graphs(lineCount, filename);
+
+                TNode<int> stack = new TNode<int>(lineCount);
+
+            }
+            catch (InvalidOperationException ex)
+            {
+                SupportMethods.Pause(ex.Message);
+            }
+
+            SupportMethods.Pause();
+
+        }
+
+        /// <summary>
+        /// 4. *Создать библиотеку функций для работы с графами.
+        /// </summary>
+        static void Task4()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                SupportMethods.PrepareConsoleForHomeTask("1 - Task 1\n" +
+                  "2 - Task 2\n" +
+                  "3 - Task 3\n" +
+                  "4 - Task 4\n" +
+                  "0 (Esc) - Exit\n");
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                        Task1();
+                        break;
+                    case ConsoleKey.D2:
+                        Task2();
+                        break;
+                    case ConsoleKey.D3:
+                        Task3();
+                        break;
+                    case ConsoleKey.D4:
+                        Task4();
+                        break;
+                    case ConsoleKey.D0:
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
